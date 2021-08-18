be8d4725993d9ceb5df05a83d328644f9c773f5f
Max Schulkin <maxemijo@gmail.com>
2021-08-18 20:23:35 +0200

    revision of setup bash script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+# run it with sudo just to be sure
 
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+if ! command -v brew &> /dev/null
+then
+    echo "Installing Homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update && brew upgrade
+fi
+
+brew install --cask iterm2
 
 brew install --cask visual-studio-code
 
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo $(which zsh) >> /etc/shells && chsh -s $(which zsh)
+# echo $(which zsh) >> /etc/shells && chsh -s $(which zsh) # keep this commented
