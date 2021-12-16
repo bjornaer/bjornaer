79c1c156658328eb15d2374cfb9f91aed4e0b33c
Max Schulkin <maxemijo@gmail.com>
2021-12-16 15:48:08 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 # run it with sudo just to be sure
 
-if ! command -v brew &> /dev/null
-then
-    echo "Installing Homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    brew update && brew upgrade
-fi
-
-brew install --cask iterm2
-
-brew install --cask visual-studio-code
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 brew install virtualenv
 
 brew install pyenv
@@ -93,7 +79,7 @@ kv () {
 }
 EOF
 
-echo "alias grb='git branch | grep -v "master\|main\|develop" | xargs git branch -D'" >> ~/.zshrc
+echo `alias grb='git branch | grep -v "master\|main\|develop" | xargs git branch -D'` >> ~/.zshrc
 
 # source <(kubectl completion $(basename $SHELL))
 
